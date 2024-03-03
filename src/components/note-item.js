17ce8d2e33a54c0a9ea4974f4d44afc2dae176ce
Manuel Scholz <manuel.serrano943@gmail.com>
2024-03-03 13:07:00 +0100

    Fix: Estilos

--- src/components/note-item.js
+++ src/components/note-item.js
@@ -86,6 +86,7 @@ const styles = StyleSheet.create({
         borderWidth: 2,
         borderColor: "#fff",
         elevation: 5,
+        overflow: "hidden"
     },
     header: {
         flexDirection: "row",
--- src/components/rich-editor/header-editor.js
+++ src/components/rich-editor/header-editor.js
@@ -7,7 +7,7 @@ import { SIZES } from "./font-size";
 const fontSizeLabel = ({ openFontSize, fontSize }) =>
 (
     <View style={[styles.fontSizeLabel, { backgroundColor: openFontSize ? "rgba(255, 255, 255, 0.75)" : "transparent" }]}>
-        <Image source={require("../../../assets/font-size.png")} style={{ width: 30, height: 30 }} />
+        <Image source={require("../../../assets/font-size.png")} style={{ width: 25, height: 25 }} />
         <Text style={[ui.h4, { color: "#000" }]}>{Object.keys(SIZES).find(key => SIZES[key] === fontSize)}</Text>
     </View>
 );
@@ -36,5 +36,6 @@ const styles = StyleSheet.create({
         alignItems: "center",
         paddingHorizontal: 4,
         borderRadius: 8,
+        marginLeft: 16
     }
 })
