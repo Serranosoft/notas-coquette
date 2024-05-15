73406fdd22c501b145cb18cf1ae653e56a6729a5
Manuel Scholz <manuel.serrano943@gmail.com>
2024-05-15 13:41:31 +0200

    Fix: Estilos label activo en footer editor

--- src/utils/labels.js
+++ src/utils/labels.js
@@ -1,12 +1,12 @@
 import { Image, Text, View } from "react-native"
 
-export const boldLabel = ({tintColor}) => <View style={{ backgroundColor: tintColor, }}><Image style={common} source={require("../../assets/bold.png")} /></View>
-export const italicLabel = ({tintColor}) => <View style={{ backgroundColor: tintColor }}><Image style={common} source={require("../../assets/italic.png")} /></View>
-export const underlineLabel = ({tintColor}) => <View style={{ backgroundColor: tintColor }}><Image style={common} source={require("../../assets/underline.png")} /></View>
-export const alignLeftLabel = ({tintColor}) => <View style={{ backgroundColor: tintColor }}><Image style={common} source={require("../../assets/align-left.png")} /></View>
-export const alignCenterLabel = ({tintColor}) => <View style={{ backgroundColor: tintColor }}><Image style={common} source={require("../../assets/align-center.png")} /></View>
-export const alignRightLabel = ({tintColor}) => <View style={{ backgroundColor: tintColor }}><Image style={common} source={require("../../assets/align-right.png")} /></View>
-export const listLabel = ({tintColor}) => <View style={{ backgroundColor: tintColor }}><Image style={common} source={require("../../assets/list.png")} /></View>
+export const boldLabel = ({tintColor}) => <View style={{ backgroundColor: tintColor, borderRadius: 8 }}><Image style={common} source={require("../../assets/bold.png")} /></View>
+export const italicLabel = ({tintColor}) => <View style={{ backgroundColor: tintColor, borderRadius: 8 }}><Image style={common} source={require("../../assets/italic.png")} /></View>
+export const underlineLabel = ({tintColor}) => <View style={{ backgroundColor: tintColor, borderRadius: 8 }}><Image style={common} source={require("../../assets/underline.png")} /></View>
+export const alignLeftLabel = ({tintColor}) => <View style={{ backgroundColor: tintColor, borderRadius: 8 }}><Image style={common} source={require("../../assets/align-left.png")} /></View>
+export const alignCenterLabel = ({tintColor}) => <View style={{ backgroundColor: tintColor, borderRadius: 8 }}><Image style={common} source={require("../../assets/align-center.png")} /></View>
+export const alignRightLabel = ({tintColor}) => <View style={{ backgroundColor: tintColor, borderRadius: 8 }}><Image style={common} source={require("../../assets/align-right.png")} /></View>
+export const listLabel = ({tintColor}) => <View style={{ backgroundColor: tintColor, borderRadius: 8 }}><Image style={common} source={require("../../assets/list.png")} /></View>
 export const separatorsLabel = () => <View style={{ marginLeft: "auto", marginRight: 4 }}><Image style={common} source={require("../../assets/icons.png")} /></View>
 export const fontSizeLabel = () => <View><Image style={[common]} source={require("../../assets/font-size.png")} /></View>
 export const colorsLabel = () => <View><Image style={[common]} source={require("../../assets/colors.png")} /></View>
