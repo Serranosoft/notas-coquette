987015c69afec0a9a30716fac45d72c5ceb5bafb
Manuel Scholz <manuel.serrano943@gmail.com>
2024-05-16 10:41:40 +0200

    Fix: Comprobar si la nota a actualizar contiene diferencias y actualizarla únicamente en ese caso

--- src/utils/storage.js
+++ src/utils/storage.js
@@ -8,15 +8,33 @@ export async function save({ note, noteSavedId }) {
         }
     
         const oldNote = notes.find((oldNote) => oldNote.id === noteSavedId);
+
+        // Si existe una oldNote, debo saber si debo actualizarla o no.
         if (oldNote) {
-            oldNote.content = note.content;
-            oldNote.date = new Date().getTime();
-            oldNote.pwd = note.pwd;
-            oldNote.color = note.color
+            let differences = 0;
+
+            if (oldNote.content !== note.content) {
+                differences++;
+            }
+            if (oldNote.hasOwnProperty("pwd") && note.hasOwnProperty("pwd") && oldNote.pwd !== note.pwd) {
+                differences++;
+            }
+            if (oldNote.hasOwnProperty("color") && note.hasOwnProperty("color") && oldNote.color !== note.color) {
+                differences++;
+            }
+
+            if (differences > 0) {
+                oldNote.content = note.content;
+                oldNote.date = new Date().getTime();
+                oldNote.pwd = note.pwd;
+                oldNote.color = note.color
+            } else {
+                return false;
+            }
         } else {
             notes.push(note);
         }
-    
+
         const jsonValue = JSON.stringify(notes);
         await AsyncStorage.setItem("notes", jsonValue);
 
